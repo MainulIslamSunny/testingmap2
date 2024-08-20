a15914a1d858abd1a12bc24c2c2c8d032b45387b
Mainul <s.m.sunny97@gmail.com>
2024-08-20 14:28:08 +0600

    Your descriptive commit message 3

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
+import os
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('run.html')  # This serves the run.html file
+    return render_template('run.html')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+    port = int(os.environ.get('PORT', 8080))
+    app.run(host='0.0.0.0', port=port)
